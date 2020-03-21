768eb18b3ea563f580e3c9664147b39ffb87855f
Jack Wohlfert <jack.wohlfert@gmail.com>
2020-03-21 12:27:29 -0400

    Initial Commit

---
+++ handler.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+
+exports.downloadPdf = async (event) => {
+  const { url } = event.queryStringParameters;
+
+  const executablePath = event.isOffline
+    ? '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    : await chromium.executablePath;
+
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(url, {
+    waitUntil: ['networkidle0', 'load', 'domcontentloaded'],
+  });
+
+  const pdf = await page.pdf();
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/pdf' },
+    body: pdf.toString('base64'),
+    isBase64Encoded: true,
+  };
+};
